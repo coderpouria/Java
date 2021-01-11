a7c75aca0ddc1e9ab714ebeb5deb3fab4080d2a9
Pouria Teymouri <77165717+coderpouria@users.noreply.github.com>
2021-01-11 19:08:18 +0330

    Add Hello World

---
+++ HelloWorld/src/HelloWorldApp.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!\n");
+        System.out.println("Welcome to Java.\n");
+        System.out.println("Press enter to continue...");
+        Scanner input = new Scanner(System.in);
+        input.nextLine();
+    }
+}
